e2cb1acdc310cebd05061032814b1fa256a26ea2
Sergey <biznesgroup2017@gmail.com>
2021-08-11 18:37:36 +0300

    modified 3 and 4 tasks

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-//**********************TASK 1*******************************//
+// вернуть максимальное число
+function max(a, b, c) {
+    if (a > c && a > b) return a;
+    if (a < b && b > c) return b;
+    if (b < c) return c;
 
-
-
-
-
-const task1 = (a, b, c) => a + b + c;
-task1(1, 2, 3);
-
-//**********************TASK 2*******************************//
-
-//вернуть сумму чисел, но надо учесть что аргументы могут быть не только числами, использовать оператор typeof в связке с if-else
-
-
-const task2 = (a, b, c) => {
-   
-    if(typeof a !== 'number' && typeof b != "number") return c;
-    if(typeof a !== 'number' && typeof c !== "number") return b;
-    if(typeof b !== 'number' && typeof c !== "number") return a;
-    if(typeof a !== "number") return  b + c;
-    if(typeof b !== "number") return  a + c;
-    if(typeof c !== "number") return  a + b;
-    return a + b + c;
 
 }
 
-task2(4, "2", "2");
+console.log(max(6, 3, 1));
 
+//вернуть минимальное число
+function min(a, b, c) {
+    if (a < c && a < b) return a;
+    if (a > b && b < c) return b;
+    if (b > c) return c;
+}
 
-//**********************TASK 3*******************************//
-
-// const max = (b, x, ...maximal) => Math.max.apply(null, maximal);
-
-// console.log(max(2,3,4));
-
-//**********************TASK 4*******************************//
-
-const min = (a, b, ...minimal) => Math.min(minimal);
-
-
-console.log(min(2,3,4));
+console.log(min(1, 3, 4));
